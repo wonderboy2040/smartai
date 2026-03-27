eea3d29b6fe69c0f1cc1576ad4917e39dab91dcf
wonderboy2040 <149177815+wonderboy2040@users.noreply.github.com>
2026-03-28 02:26:55 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,11 +1,19 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
+
+export const loginAnonymously = async () => {
+  try {
+    await signInAnonymously(auth);
+  } catch (error) {
+    console.error("Error logging in anonymously", error);
+  }
+};
 
 export enum OperationType {
   CREATE = 'create',
